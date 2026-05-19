0b1a271787850503e186520aaed8578a01546239
Kevin.M <kema2501@student.miun.se>
2026-05-19 07:53:47 +0200

    Skapa reservation schema

---
+++ models/reservation.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+// Reservation schema
+const reservationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String, 
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    guests: {
+        type: Number,
+        required: true,
+        min: [1]
+    },
+    allergies: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ["newBooking", "confirmed", "cancelled"],
+        default: "newBooking"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Reservation = mongoose.model("Reservation", reservationSchema);
+module.exports = Reservation; 
