62524ceea088a8f543058965e5a2b24079770d09
Kevin.M <kema2501@student.miun.se>
2026-05-26 07:35:52 +0200

    Lägg till kontoll av tid i reservationschema

--- models/reservation.js
+++ models/reservation.js
@@ -13,12 +13,33 @@ const reservationSchema = new mongoose.Schema({
     },
     date: {
         type: Date,
-        required: true
+        required: true,
+        validate: [
+      {
+        // DATE VALIDATION (Today or in the future)
+        validator: function(value) {
+          const today = new Date();
+          today.setHours(0, 0, 0, 0);
+          
+          return value >= today;
+        },
+        message: "You can't make reservations for dates that have past."
+      },
+      {
+        // TIME VALIDATION
+        validator: function(value) {
+          const hour = value.getHours();
+          
+          return hour >= 15 && hour <= 22;
+        },
+        message: "Bookings can only be made for times between 15:00 and 22:00."
+      }
+    ]
     },
     guests: {
         type: Number,
         required: true,
-        min: [1]
+        min: [1, 'Minimum 1 guest required.']
     },
     requests: {
         type: String
