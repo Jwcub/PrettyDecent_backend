5843e4036a3d3f552229bb5ccec478bdf77d1f85
Kevin.M <kema2501@student.miun.se>
2026-06-04 11:29:50 +0200

    Ändra valideringstjänst för kontroll av tid från kontrolla av date till sträng

--- models/reservation.js
+++ models/reservation.js
@@ -12,27 +12,28 @@ const reservationSchema = new mongoose.Schema({
         trim: true
     },
     date: {
-        type: Date,
+        type: String,
         required: true,
         validate: [
       {
         // DATE VALIDATION
-        validator: function(value) {
-          const today = new Date();
-          today.setHours(0, 0, 0, 0);
-          
-          return value >= today;
+        validator: function(date) {
+          const bookingDate = new Date(date);
+          const now = new Date();
+          return bookingDate >= now;
         },
         message: "Din bokning måste avse ett datum frammåt i tiden"
       },
       {
         // TIME VALIDATION
-        validator: function(value) {
-          const hour = value.getHours();
-          
-          return hour >= 15 && hour <= 22;
+        validator: function(date) {
+          const timePart = v.split('T')[1]; 
+          if (!timePart) return false;
+
+          const hour = parseInt(timePart.split(':')[0], 10);
+          return hour >= 15 && hour < 22;
         },
-        message: "Det går endast att boka bord för tider mellan 15:00 och 22:00."
+        message: 'Bokningstiden måste vara mellan 15:00 och 22:00.'
       }
     ]
     },
